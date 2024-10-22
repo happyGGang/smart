be2e63c28bef08734bc377d34f79660d746ed052
yanghj313 <lawork313@gmail.com>
2024-10-22 19:10:36 +0900

    [feat] 키워드 추천 세팅

---
+++ resources/ict/js/type_b/keyword.js
@@ -0,0 +1,36 @@
+$(function () {
+	// 게시판
+	$('.reco_tab a').click(function () {
+		var tab_id = $(this).attr('data-tab');
+
+		$('.reco_tab a').removeClass('active');
+		$('.tab_content').removeClass('active');
+
+		$(this).addClass('active');
+		$('#' + tab_id).addClass('active');
+	});
+});
+
+$(function () {
+	// 게시판
+	$('.recom_tab a').click(function () {
+		var tab_id = $(this).attr('data-tab');
+
+		$('.recom_tab a').removeClass('active');
+		$('.tab_contentt').removeClass('active');
+
+		$(this).addClass('active');
+		$('#' + tab_id).addClass('active');
+	});
+});
+
+$(function () {
+	var swiper = new Swiper('.swiper-container', {
+		loop: true,
+		slidesPerView: 1,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
+});
