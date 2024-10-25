6780caf92b0de28fcd9345089c0dcf2c43451ee4
yanghj313 <lawork313@gmail.com>
2024-10-25 11:25:13 +0900

    [update] B타입 푸터 수정

--- resources/ict/js/type_b/common.js
+++ resources/ict/js/type_b/common.js
@@ -1,32 +1,50 @@
 function adjustPersonWrapLeft() {
     const personWrap = document.querySelector('.person_wrap');
-    const loginDiv = document.querySelector('.login');
+    const loginDiv = document.querySelector('.user');
     const screenWidth = window.innerWidth;
+
+    let loginWidth = 0;  
+    let extraWidth = 0; 
+
+    if (loginDiv) {
+        loginWidth = loginDiv.getBoundingClientRect().width;
+
+        
+        if (screenWidth === 3840) {
+            extraWidth = 54;
+        } else if (screenWidth === 1920) {
+            extraWidth = 54; 
+        } else if (screenWidth === 1080) {
+            extraWidth = 27; 
+        } else if (screenWidth === 2160) {
+            extraWidth = 27; 
+        }
+    }
 
     if (screenWidth === 3840) {
         if (loginDiv) {
-            personWrap.style.left = '2380px';
-        } else {
-            personWrap.style.left = '2878px';
+        personWrap.style.left = loginDiv ? `${2878 - loginWidth - extraWidth}px` : '2380px';
+        }else{
+         personWrap.style.left = '2878px';  
         }
     } else if (screenWidth === 2160) {
         if (loginDiv) {
-            personWrap.style.left = '33%';
-        } else {
-            personWrap.style.left = '55%';
+        personWrap.style.left = loginDiv ? `calc(55% - ${loginWidth + extraWidth}px)` : '33%';
+        }else{
+            personWrap.style.left = '55%';  
         }
     } else if (screenWidth === 1920) {
         if (loginDiv) {
-            personWrap.style.left = '1190px';
-        } else {
-            personWrap.style.left = '1439px';
+        personWrap.style.left = loginDiv ? `${1439 - loginWidth - extraWidth}px` : '1190px';
+        }else{
+            personWrap.style.left = '1439px';    
         }
     } else if (screenWidth === 1080) {
         if (loginDiv) {
-            personWrap.style.left = '33%';
-        } else {
-            personWrap.style.left = '55%';
-        }
+            personWrap.style.left = loginDiv ? `calc(55% - ${loginWidth + extraWidth}px)` : '33%';
+            }else{
+                personWrap.style.left = '55%';  
+            }
     }
 }
 
