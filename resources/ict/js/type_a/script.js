5fe7e4276f3fd02c00cc1fe8788494d8624c6f02
infosetGang <kangyoonkyung@infoset.co.kr>
2024-11-04 15:22:29 +0900

    [feat] 스마트 도서 추천 a 타입 메인, 키워드 추천 결과 페이지 세로 작업 완료

--- resources/ict/js/type_a/script.js
+++ resources/ict/js/type_a/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(window).on('load', function () {
 	const $navigationMenus = $('.common_navigation_menu');
 
 	$navigationMenus.removeClass('common_navigation_menu_active').first().addClass('common_navigation_menu_active');
@@ -6,5 +6,25 @@ $(document).ready(function () {
 	$navigationMenus.on('click', function () {
 		$navigationMenus.removeClass('common_navigation_menu_active');
 		$(this).addClass('common_navigation_menu_active');
+	});
+});
+
+$(function () {
+	const keywordSwiper = new Swiper('.keyword_result_swiper', {
+		speed: 600,
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'fraction',
+		},
+		loop: true,
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true,
+		},
+		slidesPerView: 1,
+		navigation: {
+			nextEl: '.keyword_result_swiper .swiper-button-next',
+			prevEl: '.keyword_result_swiper .swiper-button-prev',
+		},
 	});
 });
